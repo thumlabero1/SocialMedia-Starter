9af725debb2701f5e49d696d846fa172d61db830
Phan_Linh <nhomtoanhsg20172018@gmail.com>
2022-06-26 20:58:37 +0700

    yes

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './Profile.css'
+const Profile = () =>
+    {
+        return(
+            <div className="Profile">lf</div>
+        )
+    }
+export default Profile
