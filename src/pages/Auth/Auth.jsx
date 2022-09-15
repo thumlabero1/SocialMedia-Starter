b45c67e58944e21caa24bebc4e4fab1467160e88
Phan_Linh <nhomtoanhsg20172018@gmail.com>
2022-09-15 14:34:28 +0700

    15/9

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Auth.css'
-import logo from '../../img/logo.png'
+//import logo from '../../img/logo.png'
 export const Auth = () => {
   return (
     <div className="Auth">
